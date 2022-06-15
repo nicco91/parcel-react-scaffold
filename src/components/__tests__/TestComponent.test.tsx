ffb3b844772e06909374519b59b5af79851485de
Niccolò Taddei <n.taddei91@gmail.com>
2022-06-15 15:19:04 +0200

    Removed unused libraries

--- src/components/__tests__/TestComponent.test.tsx
+++ src/components/__tests__/TestComponent.test.tsx
@@ -3,8 +3,13 @@ import { render } from '@testing-library/react';
 import TestComponent from '../TestComponent';
 
 describe('TestComponent', () => {
-  test('should render', () => {
+  it('should render', () => {
     const { getByText } = render(<TestComponent />);
     expect(getByText('TestComponent')).toBeInTheDocument();
+  });
+
+  it('should match the snapshot', () => {
+    const { container } = render(<TestComponent />);
+    expect(container).toMatchSnapshot();
   });
 });
